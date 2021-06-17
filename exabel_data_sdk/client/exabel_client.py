5e36b2172deb13b08008ed540220a4d505bcff89
HaakonHam <haakon.hammarbeck@gmail.com>
2021-06-17 15:49:52 +0200

    Add optional certificates file in exabel client

--- exabel_data_sdk/client/exabel_client.py
+++ exabel_data_sdk/client/exabel_client.py
@@ -17,21 +17,23 @@ class ExabelClient:
         host: str = None,
         port: int = None,
         timeout: int = None,
+        root_certificates: str = None,
     ):
         """
         Initialize a new client.
 
         Args:
-            api_key:        API key to use. If not set, the API key must be set using the
-                            environment variable EXABEL_API_KEY. If set to the value 'NO_KEY',
-                            the client will use an insecure channel, typically used for local
-                            testing.
-            client_name:    Override name of this client. Default name is "Exabel Python SDK".
-            host:           Override default Exabel API host.
-            port:           Override default Exabel API port.
-            timeout:        Override default timeout in seconds to use for API requests.
+            api_key:            API key to use. If not set, the API key must be set using the
+                                environment variable EXABEL_API_KEY. If set to the value 'NO_KEY',
+                                the client will use an insecure channel, typically used for local
+                                testing.
+            client_name:        Override name of this client. Default name is "Exabel Python SDK".
+            host:               Override default Exabel API host.
+            port:               Override default Exabel API port.
+            timeout:            Override default timeout in seconds to use for API requests.
+            root_certificates:  Root certificates to use.
         """
-        config = ClientConfig(api_key, client_name, host, port, timeout)
+        config = ClientConfig(api_key, client_name, host, port, timeout, root_certificates)
 
         self.entity_api = EntityApi(config)
         self.signal_api = SignalApi(config)
