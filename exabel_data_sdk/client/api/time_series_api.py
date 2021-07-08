4c1419dd46879b112d089ed35cbf4bb39801f1a9
Øyvind Matheson Wergeland <wergeland@exabel.com>
2021-07-08 15:48:09 +0200

    Expose create_library_signal and create_tag (#33)

--- exabel_data_sdk/client/api/time_series_api.py
+++ exabel_data_sdk/client/api/time_series_api.py
@@ -110,7 +110,7 @@ class TimeSeriesApi:
 
         return self._time_series_points_to_series(time_series.points, time_series.name)
 
-    def create_time_series(self, name: str, series: pd.Series) -> None:
+    def create_time_series(self, name: str, series: pd.Series, create_tag: bool = False) -> None:
         """
         Create a time series.
 
@@ -119,19 +119,23 @@ class TimeSeriesApi:
         first version is the canonical form. The signal must be associated with the entity's type.
 
         Args:
-            name:   The resource name of the time series, for example
-                    "entityTypes/ns1.type/entities/ns2.entities/signals/ns3.signal".
-                    An alternative name for the same time series is
-                    "signals/ns3.signal/entityTypes/ns1.type/entities/ns2.entity". The namespaces
-                    must be empty (being global) or one of the predetermined namespaces the
-                    customer has access to. If ns2 is not empty, it must be equals to ns3,
-                    and if ns1 is not empty, all three namespaces must be equal.
-            series: The time series data
+            name:       The resource name of the time series, for example
+                        "entityTypes/ns1.type/entities/ns2.entities/signals/ns3.signal".
+                        An alternative name for the same time series is
+                        "signals/ns3.signal/entityTypes/ns1.type/entities/ns2.entity". The
+                        namespaces must be empty (being global) or one of the predetermined
+                        namespaces the customer has access to. If ns2 is not empty, it must be
+                        equals to ns3, and if ns1 is not empty, all three namespaces must be equal.
+            series:     The time series data
+            create_tag: Set to true to create a tag for every entity type a signal has time series
+                        for. If a tag already exists, it will be updated when time series are
+                        created (or deleted) regardless of the value of this flag.
         """
         time_series_points = self._series_to_time_series_points(series)
         self.client.create_time_series(
             CreateTimeSeriesRequest(
-                time_series=ProtoTimeSeries(name=name, points=time_series_points)
+                time_series=ProtoTimeSeries(name=name, points=time_series_points),
+                create_tag=create_tag,
             ),
         )
 
