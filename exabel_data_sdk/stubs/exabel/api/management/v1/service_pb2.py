af9448db5567aa5ec0e61872c7b37ca5cab78cbf
Aksel Stokseth <36400733+aksestok@users.noreply.github.com>
2022-03-15 14:34:26 +0100

    Copy proto stubs (#77)

---
+++ exabel_data_sdk/stubs/exabel/api/management/v1/service_pb2.py
@@ -0,0 +1,10 @@
+"""Generated protocol buffer code."""
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from google.protobuf import reflection as _reflection
+from google.protobuf import symbol_database as _symbol_database
+_sym_db = _symbol_database.Default()
+from .....protoc_gen_openapiv2.options import annotations_pb2 as protoc__gen__openapiv2_dot_options_dot_annotations__pb2
+DESCRIPTOR = _descriptor.FileDescriptor(name='exabel/api/management/v1/service.proto', package='exabel.api.management.v1', syntax='proto3', serialized_options=b'\n\x1ccom.exabel.api.management.v1B\x0cServiceProtoP\x01Z\x1cexabel.com/api/management/v1\x92A\x9b\x02\x12U\n\x15Exabel Management API"5\n\x06Exabel\x12\x17https://www.exabel.com/\x1a\x12support@exabel.com2\x051.0.0\x1a\x19management.api.exabel.com*\x01\x022\x10application/json:\x10application/jsonZ\x1c\n\x1a\n\x07API key\x12\x0f\x08\x02\x1a\tx-api-key \x02b\r\n\x0b\n\x07API key\x12\x00rS\n$More about the Exabel Management API\x12+https://help.exabel.com/docs/management-api', create_key=_descriptor._internal_create_key, serialized_pb=b'\n&exabel/api/management/v1/service.proto\x12\x18exabel.api.management.v1\x1a.protoc_gen_openapiv2/options/annotations.protoB\xeb\x02\n\x1ccom.exabel.api.management.v1B\x0cServiceProtoP\x01Z\x1cexabel.com/api/management/v1\x92A\x9b\x02\x12U\n\x15Exabel Management API"5\n\x06Exabel\x12\x17https://www.exabel.com/\x1a\x12support@exabel.com2\x051.0.0\x1a\x19management.api.exabel.com*\x01\x022\x10application/json:\x10application/jsonZ\x1c\n\x1a\n\x07API key\x12\x0f\x08\x02\x1a\tx-api-key \x02b\r\n\x0b\n\x07API key\x12\x00rS\n$More about the Exabel Management API\x12+https://help.exabel.com/docs/management-apib\x06proto3', dependencies=[protoc__gen__openapiv2_dot_options_dot_annotations__pb2.DESCRIPTOR])
+_sym_db.RegisterFileDescriptor(DESCRIPTOR)
+DESCRIPTOR._options = None
