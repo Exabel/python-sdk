de5ded913bc0abea5bd6e58e553026a33d755a29
Aksel Stokseth <36400733+aksestok@users.noreply.github.com>
2022-03-15 13:16:00 +0100

    Support null as property value (#75)

--- exabel_data_sdk/client/api/proto_utils.py
+++ exabel_data_sdk/client/api/proto_utils.py
@@ -8,7 +8,7 @@ def from_struct(struct: Struct) -> Mapping[str, Union[str, bool, int, float]]:
     Convert a protobuf Struct into a Dict.
     """
     for value in struct.values():
-        if not isinstance(value, (str, bool, int, float)):
+        if not isinstance(value, (str, bool, int, float, type(None))):
             raise ValueError(f"Struct contains unsupported value: {value}")
     return dict(struct.items())
 
