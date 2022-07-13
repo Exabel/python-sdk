af92933f2acd7df2515072a35ff3d816d9cd99f3
BoHuang2018 <tigerhuangbo@icloud.com>
2022-07-13 12:56:02 +0200

    updated SDK with monorepo commit ed52fd5b5f3ebaaf44100af65d7f9099f7ad2fa1 of PR: Make unittest work on both monorepo and sdk repo (#9114)

--- exabel_data_sdk/tests/services/test_csv_time_series_loader.py
+++ exabel_data_sdk/tests/services/test_csv_time_series_loader.py
@@ -16,20 +16,14 @@ class TestCsvTimeSeriesLoader(unittest.TestCase):
                 namespace="test",
             )
         exception = context.exception
-        expected_error_message_beginning = (
+        actual = str(exception)
+        self.assertIn(
             "2 signal column(s) contain non-numeric values. "
-            "Please ensure all values can be parsed to numeric values\n\n\n"
-            "Signal 'production' contains 6 non-numeric values, check the first five as examples:\n"
-            "     entity       date known_time      production\n"
-            "small_brand 2022-02-01 2022-02-01 three_thousands\n"
-            "small_brand 2022-02-04 2022-02-05  four_thousands\n "
-            "mini_brand 2022-02-05 2022-02-06  five_thousands\n "
-            "mini_brand 2022-02-06 2022-02-07   six_thousands\n "
-            "mini_brand 2022-02-07 2022-02-08 seven_thousands\n\n"
-            "Signal 'price' contains 3 non-numeric values:\n      "
-            "entity       date known_time     price\n"
-            "middle_brand 2022-02-01 2022-02-01   fifteen\n"
-            "middle_brand 2022-01-01 2022-01-01   sixteen\n "
-            "small_brand 2022-02-02 2022-02-03 seventeen"
+            "Please ensure all values can be parsed to numeric values",
+            actual,
         )
-        self.assertEqual(expected_error_message_beginning, str(exception))
+        self.assertIn(
+            "Signal 'production' contains 6 non-numeric values, check the first five as examples:",
+            actual,
+        )
+        self.assertIn("Signal 'price' contains 3 non-numeric values", actual)
